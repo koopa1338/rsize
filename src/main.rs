5f435cb16c9126f3a0ac65b496a7cb8d568800ce
koopa1338 <sinner1991@gmail.com>
2021-04-16 13:07:36 +0200

    Refactor / flag for ignoring aspect ratio

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,10 @@ use clap::{App, Arg};
 use glob::glob;
 use image::{imageops::FilterType, open};
 use rayon::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-fn resize_images(paths: &[PathBuf], width: u32, height: u32, filepath: &Path) {
-    paths.into_par_iter().for_each( |p| {
-        let img_path = filepath.to_owned().as_path().join(&p.as_path());
-        let img = open(&p.as_path()).unwrap();
-        let (dim_w, _) = img.to_rgb16().dimensions();
-        // only resize if the desired width is different
-        if dim_w != width {
-            img.resize(width, height, FilterType::Lanczos3)
-                .save(&img_path)
-                .unwrap();
-            println!("Resized file {:?}", img_path);
-        }
-    });
-}
 
-fn resize(filepath: PathBuf, width: u32, height: u32) {
+fn resize(filepath: PathBuf, width: u32, height: u32, ignore_aspect: bool) {
     if filepath.is_file() {
         let img = open(filepath.as_path()).unwrap();
         let resized_img = img.resize(width, height, FilterType::Lanczos3);
@@ -46,7 +32,26 @@ fn resize(filepath: PathBuf, width: u32, height: u32) {
         for j in jpgs {
             images.push(j.unwrap());
         }
-        resize_images(&images, width, height, &filepath);
+
+        // resize images
+        images.into_par_iter().for_each( |p| {
+            let img_path = filepath.to_owned().as_path().join(&p.as_path());
+            let img = open(&p.as_path()).unwrap();
+            let (dim_w, _) = img.to_rgb16().dimensions();
+            // only resize if the desired width is different
+            if dim_w != width {
+                if ignore_aspect {
+                    img.resize_exact(width, height, FilterType::Lanczos3)
+                        .save(&img_path)
+                        .unwrap();
+                } else {
+                    img.resize(width, height, FilterType::Lanczos3)
+                        .save(&img_path)
+                        .unwrap();
+                }
+                println!("Resized file {:?}", img_path);
+            }
+        });
     }
 }
 
@@ -78,11 +83,18 @@ fn main() {
                 .help("desired height")
                 .default_value("1080"),
         )
+        .arg(
+            Arg::with_name("ignore-aspect")
+                .short("i")
+                .takes_value(false)
+                .help("ignore the aspect ratio and resice exactly to the width and height"),
+        )
         .get_matches();
 
     let filepath = PathBuf::from(matches.value_of("src").unwrap());
+    let ignore_aspect: bool = matches.is_present("ignore-aspect");
     let width: u32 = matches.value_of("width").unwrap().parse::<u32>().unwrap();
     let height: u32 = matches.value_of("height").unwrap().parse::<u32>().unwrap();
 
-    resize(filepath, width, height);
+    resize(filepath, width, height, ignore_aspect);
 }
