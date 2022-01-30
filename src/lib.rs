103ebae291e10bc00587bf3d0e29bdd4362ebb4c
koopa1338 <sinner1991@gmail.com>
2022-01-30 01:11:45 +0100

    optimizing and print better errors on panic

--- src/lib.rs
+++ src/lib.rs
@@ -2,51 +2,82 @@ use image::{imageops::FilterType, open};
 use rayon::prelude::*;
 use std::{fs::read_dir, path::PathBuf};
 
-const EXTENSIONS: [&str; 2] = ["png", "jpg"];
+use clap::Parser;
 
-pub fn resize(filepath: PathBuf, width: u32, height: u32, ignore_aspect: bool) {
-    if filepath.is_file() {
-        let img = open(filepath.as_path()).unwrap();
-        let resized_img = img.resize(width, height, FilterType::Lanczos3);
-        resized_img.save(filepath.as_path()).unwrap();
+const EXTENSIONS: [&str; 3] = ["png", "jpg", "jpeg"];
+
+#[derive(Debug, Clone, Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Config {
+    #[clap(long, short, default_value = "./", parse(from_os_str))]
+    src: PathBuf,
+    #[clap(long, short)]
+    ignore_aspect: bool,
+    #[clap(long, default_value_t = 1920u32)]
+    width: u32,
+    #[clap(long, default_value_t = 1080u32)]
+    height: u32,
+}
+
+pub fn resize(config: Config) {
+    if config.src.is_file() {
+        let filepath = config.src.as_path();
+        let img = open(filepath).unwrap_or_else(|_| {
+            panic!(
+                "Error while opening image file {:?}. Maybe corrupted?",
+                filepath
+            )
+        });
+        let resized_img = img.resize(config.width, config.height, FilterType::Lanczos3);
+        resized_img
+            .save(filepath)
+            .unwrap_or_else(|_| panic!("Error while saving resized image {:?}", filepath));
         println!("Resized file {:?}", filepath);
     } else {
-        resize_all(filepath, width, height, ignore_aspect);
+        resize_all(config);
     }
 }
 
-fn resize_all(filepath: PathBuf, width: u32, height: u32, ignore_aspect: bool) {
+fn resize_all(config: Config) {
+    let filepath = config.src.as_path();
     //get all files as PathBuf in a vec
-    let all_files = read_dir(filepath.as_path())
-        .unwrap()
+    let all_files = read_dir(filepath)
+        .unwrap_or_else(|_| panic!("couldn't read souce directory {:?}", filepath))
         .filter_map(|e| e.ok())
         .map(|e| e.path())
+        .filter(|f| {
+            if let Some(extension) = f.as_path().extension() {
+                if let Some(ext) = extension.to_str() {
+                    return EXTENSIONS.contains(&ext);
+                }
+            }
+            false
+        })
         .collect::<Vec<PathBuf>>();
 
     all_files.into_par_iter().for_each(|p| {
         if p.is_file() {
-            if let Some(extension) = p.as_path().extension() {
-                if let Some(ext) = extension.to_str() {
-                    if EXTENSIONS.contains(&ext) {
-                        let img_path = filepath.to_owned().as_path().join(&p.as_path());
-                        let img = open(&p.as_path()).unwrap();
-                        let (dim_w, _) = img.to_rgb16().dimensions();
+            let img_path = filepath.join(&p.as_path());
+            let img = open(&p.as_path())
+                .unwrap_or_else(|_| panic!("Error while saving resized image {:?}", filepath));
+            let (dim_w, _) = img.to_rgb16().dimensions();
 
-                        // only resize if the desired width is different
-                        if dim_w != width {
-                            if ignore_aspect {
-                                img.resize_exact(width, height, FilterType::Lanczos3)
-                                    .save(&img_path)
-                                    .unwrap();
-                            } else {
-                                img.resize(width, height, FilterType::Lanczos3)
-                                    .save(&img_path)
-                                    .unwrap();
-                            }
-                            println!("Resized file {:?}", img_path);
-                        }
-                    }
+            // only resize if the desired width is different
+            if dim_w != config.width {
+                if config.ignore_aspect {
+                    img.resize_exact(config.width, config.height, FilterType::Lanczos3)
+                        .save(&img_path)
+                        .unwrap_or_else(|_| {
+                            panic!("Error while saving resized image {:?}", filepath)
+                        });
+                } else {
+                    img.resize(config.width, config.height, FilterType::Lanczos3)
+                        .save(&img_path)
+                        .unwrap_or_else(|_| {
+                            panic!("Error while saving resized image {:?}", filepath)
+                        });
                 }
+                println!("Resized file {:?}", img_path);
             }
         }
     });
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 use clap::Parser;
-use std::path::PathBuf;
 
-use rsize::resize;
-
-#[derive(Debug, Clone, Parser)]
-#[clap(author, version, about, long_about = None)]
-struct Config {
-    #[clap(long, short, default_value = "./", parse(from_os_str))]
-    src: PathBuf,
-    #[clap(long, short)]
-    ignore_aspect: bool,
-    #[clap(long, default_value_t = 1920u32)]
-    width: u32,
-    #[clap(long, default_value_t = 1080u32)]
-    height: u32,
-}
+use rsize::{resize, Config};
 
 fn main() -> Result<(), String> {
     let config = Config::parse();
-    resize(
-        config.src,
-        config.width,
-        config.height,
-        config.ignore_aspect,
-    );
+    resize(config);
     Ok(())
 }
